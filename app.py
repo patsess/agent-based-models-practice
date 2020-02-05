1b0dfb89c0745e6265c925ecd0f3f2808e109f5b
PatrickS <psessford@gmail.com>
2020-02-05 07:09:30 +0000

    WIP: trying Heroku deployment

---
+++ app.py
@@ -0,0 +1,11 @@
+
+import sys
+from pathlib import Path
+module_path = str(Path(__file__).absolute().parent)
+if module_path not in sys.path:
+    sys.path.append(module_path)  # e.g. '.../repos/<name_of_this_repo>'
+
+from agentbasedmodelspractice.office_training.run import server
+
+
+server.launch()
